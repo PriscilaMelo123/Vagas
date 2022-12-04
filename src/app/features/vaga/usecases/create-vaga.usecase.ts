e64bebb7d5afb54212f9ec9d4a9869c641590f9d
Priscila Melo <melo.priscilatais@gmail.com>
2022-12-04 18:34:27 -0300

    commit 1

---
+++ src/app/features/vaga/usecases/create-vaga.usecase.ts
@@ -0,0 +1,38 @@
+import { UserRepository } from "../../user/repositories/user.repository";
+import { VagaModel } from '../../../models/vaga.model'
+import { VagaRepository } from "../repositories/vaga.repository";
+
+interface CreateVagaDTO {
+  descricao: string;
+  empresa: string;
+  dtLimite: Date;
+  indAtivo: boolean;
+  maxCandidatos?: number;
+  idRecrutador: string;
+}
+
+export class CreateVagaUseCase {
+  public async execute(data: CreateVagaDTO) {
+    const usuarioRepository = new UserRepository();
+    const usuarioResult = await usuarioRepository.get(data.idRecrutador);
+
+    if(!usuarioResult) {
+      return null
+    }
+
+    const vaga = new VagaModel(
+      data.descricao,
+      data.empresa,
+      data.dtLimite,
+      data.indAtivo,
+      usuarioResult,
+      data.maxCandidatos
+    )
+
+    const repository = new VagaRepository();
+    const result = await repository.create(vaga);
+
+    return result.toJson();
+    
+  }
+}
